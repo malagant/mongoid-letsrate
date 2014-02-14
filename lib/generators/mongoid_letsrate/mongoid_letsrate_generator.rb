038ada7ec90b27bf13913bade79d3a795fdc9863
Michael Johann <mjohann@rails-experts.com>
2014-02-14 12:41:45 +0100

    Generator Fix

--- lib/generators/mongoid_letsrate/mongoid_letsrate_generator.rb
+++ lib/generators/mongoid_letsrate/mongoid_letsrate_generator.rb
@@ -1,4 +1,4 @@
-class MongoidLetsrateGenerator < Rails::Generators::Base
+class MongoidLetsrateGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   desc 'copying jquery.raty files to assets directory ...'
